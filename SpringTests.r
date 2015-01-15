1749fc781597fa47e16857d5f34b844bc9257456
Douglas Kelley <douglas.kelley@mq.edu.au>
2015-01-15 15:06:30 +1100

    tidyng up whats been plotted

--- SpringTests.r
+++ SpringTests.r
@@ -17,12 +17,27 @@ Fw=1; m=1; k=1;
 ##########################################################################################
 ## Open Figure                                                                          ##
 ##########################################################################################
-pdf("Springs.pdf",height=12,width=12)
-par(mfrow=c(4,2))
+pdf("Springs.pdf",height=12,width=24)
+par(mfrow=c(4,4))
 
+###########################################################################################
+## Run tests                                                                             ##
+###########################################################################################
+
+## For each spring type, run 4 tests:
 springDynamicss <- function(...) {
+    # 1. constant force; no viscocity
     springDynamics(...)
+    # 2. dampened force; no viscocity
     springDynamics(waterFun='Dampend',...)
+    # 3. constant force, with viscocity
+    springDynamics(visocityFun='SpatiallyConstant',...)
+    # 4. dampened force, with viscocity
+    springDynamics(waterFun='Dampend',visocityFun='SpatiallyConstant',...)
+    # 3. constnat force, with varying viscocity
+    springDynamics(visocityFun='SpatiallyVarying',...)
+    # 3. dampened force, with varying viscocity
+    springDynamics(waterFun='Dampend',visocityFun='SpatiallyVarying',...)
 }
 
 ## Hookes 1 spring ##
