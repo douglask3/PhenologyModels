5360ddac62ab62826383057fe462340375de6fdc
Douglas Kelley <douglas.kelley@mq.edu.au>
2015-01-15 16:53:46 +1100

    tidying comments and passing cols and line type and adding enough cols and line types fir whats being plotted

--- SpringTests.r
+++ SpringTests.r
@@ -11,14 +11,22 @@ sourceAllLibs("forces/viscous")
 ##########################################################################################
 ## Set up parameters                                                                    ##
 ##########################################################################################
+## For model
 nt=400; dt=0.1;
 Fw=1; m=1; k=1;
+
+## For plot
+cols=c("green","brown","yellow","blue","cyan","cyan",'cyan')
+ltys=c(1,1,1,1,2,3,4)
+
 
 ##########################################################################################
 ## Open Figure                                                                          ##
 ##########################################################################################
-pdf("Springs.pdf",height=12,width=24)
-par(mfrow=c(4,4))
+pdf("Springs.pdf",height=18,width=18)
+lmat=matrix(1:(6*4),6,4)
+lmat=rbind(lmat,rep((6*4)+1,4))
+layout(lmat,height=c(rep(1,6),0.7))
 
 ###########################################################################################
 ## Run tests                                                                             ##
@@ -26,18 +34,19 @@ par(mfrow=c(4,4))
 
 ## For each spring type, run 4 tests:
 springDynamicss <- function(...) {
+    FUN <- function(...) springDynamics(ltys=ltys,cols=cols,...)
     # 1. constant force; no viscocity
-    springDynamics(...)
+    FUN(...)
     # 2. dampened force; no viscocity
-    springDynamics(waterFun='Dampend',...)
+    FUN(waterFun='Dampend',...)
     # 3. constant force, with viscocity
-    springDynamics(visocityFun='SpatiallyConstant',...)
+    FUN(visocityFun='SpatiallyConstant',...)
     # 4. dampened force, with viscocity
-    springDynamics(waterFun='Dampend',visocityFun='SpatiallyConstant',...)
-    # 3. constnat force, with varying viscocity
-    springDynamics(visocityFun='SpatiallyVarying',...)
-    # 3. dampened force, with varying viscocity
-    springDynamics(waterFun='Dampend',visocityFun='SpatiallyVarying',...)
+    FUN(waterFun='Dampend',visocityFun='SpatiallyConstant',...)
+    # 5. constnat force, with varying viscocity
+    FUN(visocityFun='SpatiallyVarying',...)
+    # 6. dampened force, with varying viscocity
+    FUN(waterFun='Dampend',visocityFun='SpatiallyVarying',...)
 }
 
 ## Hookes 1 spring ##
@@ -52,4 +61,10 @@ springDynamicss(springFUN='exponentialSpring')
 ## Non-Linear ##
 springDynamicss(TRUE,springFUN='exponentialSpring')
 
+plot.new()
+
+legend('topleft',legend=c('Phenology','Inertia','Acceleration',"Overall Force",
+                          "Water forcing","Resistance Force","Viscous Force"),
+                          lty=ltys,col=cols,horiz=TRUE,xpd=TRUE)
+                          
 dev.off()
