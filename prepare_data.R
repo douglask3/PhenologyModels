35fbca0240bef02373c6a28248a8cac3b98d6b07
Rhys Whitley <rhys.whitley@gmail.com>
2015-01-19 16:17:53 +1100

    fixed data preparation script

--- prepare_data.R
+++ prepare_data.R
@@ -4,27 +4,29 @@ outFile <- 'outputs/ecDaily.csv'
 
 if (file.exists(outFile)) read.csv(outFile) else {
 
-    ecdata	<- read.csv(file=SturtPlainsFile,
-	    	   header=T, row.names=NULL)
+    ecdata <- sqldf::read.csv.sql(file=SturtPlainsFile,
+           sql="SELECT * FROM file",
+           header=T, row.names=NULL, 
+           filter = list('gawk -f prog', prog = '{ gsub(/"/, ""); print }'))
 
     for( i in 1:length(names(ecdata))){
 	    ecn <- names(ecdata)
     	print( paste(i,": ", ecn[i], sep=" ") )
     }
 
-    isplitstr <- function(x,f,...) t(sapply(x,function(i,...) strsplit(i,...)[[1]],f,...))
-    datetime  <- isplitstr( ecdata$DT, " " )
-    date	  <- isplitstr( datetime[,1], "-" )
-    time	  <- isplitstr( datetime[,2], ":" )
+    datetime <- stringr::str_split( ecdata$DT, " " )
+    date <- stringr::str_split( sapply( datetime, '[', 1 ), "-" )
+    time <- stringr::str_split( sapply( datetime, '[', 2 ), ":" )
 
     # reapply time columns
-    ecdata$Year	 <- as.numeric(date[,1])
-    ecdata$Month <- as.numeric(date[,2])
-    ecdata$Day	 <- as.numeric(date[,3])
+    get.tindex <- function( x, i ) as.numeric( sapply( x, '[', i) )
+    ecdata$Year <- get.tindex(date,1)
+    ecdata$Month <- get.tindex(date,2)
+    ecdata$Day <- get.tindex(date,3)
 
-    ecdata$Hour	  <- as.numeric(time[,1])
-    ecdata$Minute <- as.numeric(time[,2])
-    ecdata$Second <- as.numeric(time[,3])
+    ecdata$Hour <- get.tindex(time,1)
+    ecdata$Minute <- get.tindex(time,2)
+    ecdata$Second <- get.tindex(time,3)
 
     ecdata_daily <- aggregate( cbind(Sws_Con,X250m_16_days_NDVI_new_smooth)~Day+Month+Year, data=ecdata, mean)
 
