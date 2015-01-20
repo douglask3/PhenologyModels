e6ee07230b9437f0148d6256bb47eaa6af8a9cad
rhyswhitley <rhys.whitley@gmail.com>
2015-01-20 11:42:46 +1100

    fixed the savepath for figures in the Fe finder function && added default repo to hit CRAN in lib/install_and_source otherwise fails on command line (in ArchLinux)

--- find_Fe_function.R
+++ find_Fe_function.R
@@ -2,7 +2,7 @@
 
 # Where to save the output PDF for the preliminary fitting
 outFile <- "outputs/Fe_k_Table.csv"
-outPlot <- "outputs/Fe_from_NDVI_and_SWC.pdf"
+outPlot <- "figs/Fe_from_NDVI_and_SWC.pdf"
 
 # Import our trimmed-down Sturt Plains dataset
 ecdata <- read.csv( file="outputs/ecDaily.csv", header=T, sep="," )
--- libs/install_and_source.r
+++ libs/install_and_source.r
@@ -1,7 +1,7 @@
 install_and_source_library <- function(library_name) {
 
     if ( library_name %in% rownames(installed.packages()) == FALSE) {
-		install.packages(library_name)
+		install.packages(library_name, repos="http://cran.rstudio.com/" )
 	}
     
     library(library_name,character.only =TRUE)
