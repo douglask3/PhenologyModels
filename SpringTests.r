48c2f8e85dfee856f556344ee88324c8ef2fcb7e
douglask3 <douglas.kelley@students.mq.ed.au>
2015-01-22 11:33:52 +1100

    fixing up draft plot and fitting Sping equations

--- SpringTests.r
+++ SpringTests.r
@@ -9,27 +9,40 @@ source("springDynamics.r")
 ## Set up parameters                                                                    ##
 ##########################################################################################
 ## For plot
+graphics.off()
 cols=c("green","brown","yellow","blue","cyan","cyan",'cyan')
 ltys=c(1,1,1,1,2,3,4)
 
 ## Data
 source("find_Fe_function.R")
-force.env <-ecdata[,c('SWC10','NDVI250X')]
 
-force.env$Fe=predict(res.sig,list(SWC10=force.env$SWC10))
+ecdata$Fe=predict(res.sig,list(SWC10=ecdata$SWC10))
 
-browser()
-
+fyear = ecdata$Year + (ecdata$Month-1)/12 +ecdata$Day/365
 
 ##########################################################################################
-## Open Figure                                                                          ##
+## Open Figure and plot data                                                            ##
 ##########################################################################################
 pdf("figs/PhenologyFit.pdf",height=18,width=18)
+
+
+plot(range(fyear),c(0,0.5),type='n',xlab='',ylab='')
+mtext(side=1,'Year')
+mtext(side=2,'NDVI')
+mtext(side=3,'Soil Water Content (%)')
+
+addLine <- function(...) lines(fyear,...)
+
+addLine(ecdata$NDVI250X,col='green')
+addLine(ecdata$SWC10,col='dark blue')
+addLine(ecdata$Fe,col='red')
+addLine(ecdata$Fe,col='red')
 
 ###########################################################################################
 ## Fit and plot                                                                          ##
 ###########################################################################################
-nls(NDVI250X ~ springDynamics(Fe,r,d),start=list(r=1,d=1),data=force.env)
+fit=nls(NDVI250X ~ springDynamics(Fe,r,d,initial=ecdata$NDVI250X[1]),start=list(r=1,d=1),data=ecdata)
 
-## For each spring type, run 4 tests:
+addLine(predict(fit),col='dark green')
+
 dev.off()
