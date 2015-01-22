b7c8e5a3dd9fd042243f098bdd711bcada75f12a
douglask3 <douglas.kelley@students.mq.ed.au>
2015-01-23 10:32:57 +1100

    splitting into two plots, one fior water and one for NDVI

--- SpringTests.r
+++ SpringTests.r
@@ -17,32 +17,33 @@ ltys=c(1,1,1,1,2,3,4)
 source("find_Fe_function.R")
 
 ecdata$Fe=predict(res.sig,list(SWC10=ecdata$SWC10))
+ecdata$Fe[ecdata$Fe>1]=1
 
 fyear = ecdata$Year + (ecdata$Month-1)/12 +ecdata$Day/365
 
 ##########################################################################################
+## Fit                                                    		                        ##
+##########################################################################################
+fit=nls(NDVI250X ~ springDynamics(Fe,r,d,initial=ecdata$NDVI250X[1]),start=list(r=1,d=1),data=ecdata)
+ecdata$NDVISim=predict(fit)
+
+##########################################################################################
 ## Open Figure and plot data                                                            ##
 ##########################################################################################
-pdf("figs/PhenologyFit.pdf",height=18,width=18)
+pdf("figs/PhenologyFit.pdf",height=9,width=9)
+layout(matrix(2:1,nrow=2))
+addLine <- function(colname,col,...) lines(fyear,ecdata[,colname],col=col,...)
+plotStandard <- function(colnames,cols,xlab="",...) {
+    plot(range(fyear),c(0,max(ecdata[,colnames])),type='n',xlab=xlab,...)
+    mapply(addLine,colnames,col=cols)
+    legend('top',colnames,lty=1,horiz=TRUE,col=cols,bty='n')
+}
 
 
-plot(range(fyear),c(0,0.5),type='n',xlab='',ylab='')
-mtext(side=1,'Year')
-mtext(side=2,'NDVI')
-mtext(side=3,'Soil Water Content (%)')
+plotStandard(c('SWC10','Fe'),cols=c('dark blue','cyan'),ylab='moisture')
+plotStandard(c('NDVI250X','NDVISim'),cols=c('dark green','green'),ylab='NDVI')
 
-addLine <- function(...) lines(fyear,...)
 
-addLine(ecdata$NDVI250X,col='green')
-addLine(ecdata$SWC10,col='dark blue')
-addLine(ecdata$Fe,col='red')
-addLine(ecdata$Fe,col='red')
 
-###########################################################################################
-## Fit and plot                                                                          ##
-###########################################################################################
-fit=nls(NDVI250X ~ springDynamics(Fe,r,d,initial=ecdata$NDVI250X[1]),start=list(r=1,d=1),data=ecdata)
-
-addLine(predict(fit),col='dark green')
 
 dev.off()
